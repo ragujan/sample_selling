4656e555292f5a7467434a6423dd8100f0ed66d4
ragujan <stiflerwedontgiveup@gmail.com>
2022-10-24 12:50:17 +0530

    fix on sub sample type select

--- product_view/audio_samples/sample_display_melodies_process.php
+++ product_view/audio_samples/sample_display_melodies_process.php
@@ -7,10 +7,8 @@ require "../utils/page_buttons.php";
 // require "../PDOPHP/Validations.php";
 $query_object = new Sample_query_functions();
 $pageName = "sample_display_melodies_process";
-$pagenumber;
+
 $allowedPages = 0;
-$stopnumber = 0;
-$outputpage = 0;
 $valueforBTN = 0;
 $exactResultsPerPage = $query_object->getExactResultsPerPage();
 $DefaultSampleTypeNumber = 1;
@@ -22,7 +20,7 @@ if (isset($_POST["page_number"]) && isset($_POST["sub_sample_id"])) {
     $A = $_POST["page_number"];
     $subsampletypenumber = $_POST["sub_sample_id"];
     if ($subsampletypenumber == 0) {
-
+        echo "came here it's all";
         $valueforBTN = 0;
 
         $samplePage = $query_object->sampleTypePages($DefaultSampleTypeNumber);
@@ -36,20 +34,22 @@ if (isset($_POST["page_number"]) && isset($_POST["sub_sample_id"])) {
         $totalCount = $query_object->returnTotalCount();
         $allowedPages = ceil($totalCount / $exactResultsPerPage);
     } else {
+        echo "<br> you you";
         $valueforBTN = $subsampletypenumber;
-
+        echo "sub sample type number is ". $subsampletypenumber;
+        echo "<br> A is : ". $A;
         $samplePage = $query_object->subSampleTypePages($subsampletypenumber);
         if ($A >= $samplePage) {
             $A = $samplePage;
         } else if ($A <= 0) {
             $A = 0;
         }
-        $getRows = $query_object->sampleType($DefaultSampleTypeNumber,$A * $exactResultsPerPage);
+        $getRows = $query_object->subSampleType($subsampletypenumber,$A * $exactResultsPerPage);
         $totalCount = $query_object->returnTotalCount();
         $allowedPages = ceil($totalCount / $exactResultsPerPage);
     }
 } else if (isset($_POST["page_number"])) {
-    
+    echo "just the page number";
     $A = $_POST["page_number"];
 
     $valueforBTN = 0;
@@ -60,7 +60,7 @@ if (isset($_POST["page_number"]) && isset($_POST["sub_sample_id"])) {
         $A = 0;
     }
  
-    $getRows = $query_object->sampleType($DefaultSampleTypeNumber, $A * $exactResultsPerPage);
+    $getRows = $query_object->SampleType($DefaultSampleTypeNumber, $A * $exactResultsPerPage);
     $totalCount = $query_object->returnTotalCount();
     $allowedPages = ceil($totalCount / $exactResultsPerPage);
 } else {
