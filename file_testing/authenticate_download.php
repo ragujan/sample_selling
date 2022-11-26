76efc8e4c6fe0ff2c239a240dbe33bf72dac2114
ragujan <stiflerwedontgiveup@gmail.com>
2022-11-26 08:00:12 +0530

    enhancing the codes for download process

--- file_testing/authenticate_download.php
+++ file_testing/authenticate_download.php
@@ -1,61 +1,60 @@
 <?php
-require "DownloadLink.php";
-require "DirectoryZip.php";
 
-if(!isset($_GET["unique_id"]) || !isset($_GET["dnt"])){
-    
-    die();
-}
+
+
 $ROOT = $_SERVER["DOCUMENT_ROOT"];
 require_once $ROOT . "/sampleSelling-master/util/path_config/global_link_files.php";
 $folder_creation_path = GlobalLinkFiles::getFilePath("folder_creation");
 $style = GlobalLinkFiles::getDirectoryPath("style");
-$query_object = new DownloadLink();
+$home_page_shortend = GlobalLinkFiles::getRelativePath("home_page_shortend");
+$header_url = GlobalLinkFiles::getFilePath("header_url");
+require_once $header_url;
+require_once "Validation.php";
+
+
 $link = "http://localhost/sampleSelling-master/file_testing/authenticate_download.php?unique_id=6374abd38d577&dnt=2022-11-16%2010:22:27";
 $style_path = $style . "authenticate_download.css";
 $bootstap_path = $style . "bootstrap.css";
 $sample_selling_path = $style . "sampleselling.css";
-// if (isset($_GET["unique_id"]) && isset($_GET["dnt"])) {
+
+$unique_id;
+$dnt;
 
 
-//     $unique_id = $_GET["unique_id"];
-//     $dnt = $_GET["dnt"];
+//if it didn't receivethe request parameters through get request then redirect to homepage
+//and kill the script
+if (!isset($_GET["unique_id"]) || !isset($_GET["dnt"])) {
+
+    HeaderUrl::headerFunction($home_page_shortend);
+    die();
+}
 
 
-//     $folder_name = $folder_creation_path . uniqid() . "folder_rag";
+//make sure credentials are valid
+if (isset($_GET["unique_id"]) && isset($_GET["dnt"])) {
+    require_once "Cart.php";
+    $cart = new Cart();
+    $unique_id = $_GET["unique_id"];
+    $dnt = $_GET["dnt"];
 
-//     //make folder
+    //validate the unique id and dnt for security
+    if (!Validation::isCustomerPurchaseIdValid($unique_id) || !Validation::isDateTimeValid($dnt)) {
+        HeaderUrl::headerFunction($home_page_shortend);
+        die();
+    }
+
+
+    //make sure these are in the tables, if not redirect to homepage and kill the script
+    $status = $cart->confirmCustomerPurchase($unique_id, $dnt);
+    if (!$status) {
+        HeaderUrl::headerFunction($home_page_shortend);
+        die();
+    }
+}
 
 
 
-//     mkdir($folder_name);
 
-//     $status = $query_object->confirmCustomerPurchase($unique_id, $dnt);
-//     if ($status == 1) {
-
-//         $products = $query_object->get_products($unique_id, $dnt);
-//         $row_count = count($products);
-//         print_r($products);
-
-//         for ($i = 0; $i < $row_count; $i++) {
-//             # code...
-//             $qty = $products[$i]["qty"];
-//             for ($j = 0; $j < $qty; $j++) {
-//                 $product = $ROOT . $products[$i]["samplePath"];
-//                 echo $product;
-//                 $unique_id = $products[$i]["UniqueId"] . uniqid() . $products[$i]["Sample_Name"];
-//                 $folder_path = $folder_name . "/" . $unique_id . ".zip";
-//                 copy($product, $folder_path);
-//             }
-//         }
-//     }
-
-//     $folder_path_to_be_zipped = $ROOT . "/" . "sampleSelling-master/downloadable_zip_files/" . uniqid() . ".zip";
-//     echo $folder_path_to_be_zipped;
-//     $zip = new DirectoryZip($folder_path_to_be_zipped, $folder_name);
-// }
-$unique_id = $_GET["unique_id"];
-$dnt = $_GET["dnt"];
 
 
 
@@ -82,21 +81,32 @@ $are_you_sure = "This is an one time download link for your purchase, we don't t
     <div class="container-fluid">
 
 
-        <div  id="warning_div" class="bg-danger col-lg-6 offset-lg-3 col-10 offset-1  mt-3 py-2 px-2 abc">
+        <div id="warning_div" class="bg-danger col-lg-6 offset-lg-3 col-10 offset-1  mt-3 py-2 px-2 fw-bold warningInfoDiv">
             <div>
                 <span><b>Important! Please follow these guidance to avoid any troubles </b></span>
             </div>
             <div>
                 <ul>
 
-                    <li>Do not disable Javascript
+                    <li>Please do not share this link
                         <br>
-                        <p>We need to track the downloading status to
-                            make sure the download is successful, if Javascript is disabled
-                            we wono't be able to track and you will not get another chance
+                        <p>If someone downlaods before you, you won't be able to download the products
+                            No repayment or redownloads allowed if it is not a server side mistake
+                        </p>
+                    </li>
+                </ul>
+                <ul>
+
+
+                    <li>You can only download one time so be aware
+                        <br>
+                        <p>Once it shows where to download, that is considered as a download.
+                            So, whatever the location is just download it to your PC first
+
 
                         </p>
                     </li>
+
                 </ul>
                 <ul>
 
@@ -114,18 +124,18 @@ $are_you_sure = "This is an one time download link for your purchase, we don't t
             </div>
             <div class="text-end">
                 <div>
-                    <button onclick="acceptBtnProcess('<?=$unique_id?>','<?=$dnt?>');" id="accept-btn" class="accept-btn">accept</button>
+                    <button onclick="acceptBtnProcess('<?= $unique_id ?>','<?= $dnt ?>');" id="accept-btn" class="accept-btn">accept</button>
                 </div>
             </div>
 
         </div>
         <div class="row  p-5">
             <div id="download-form" class="col-12 d-none ">
-      
+
             </div>
         </div>
     </div>
-   
+
     <script src="script.js"></script>
 </body>
 
