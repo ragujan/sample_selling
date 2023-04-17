5268519eb0202865efa3c9b95d9140e71998bd2e
Ragujan Rag JN <stiflerwedontgiveup@gmail.com>
2023-04-17 23:36:05 +0530

    bulid: upload btn div is fixed overall

--- admin/process/show_upload_navi_btns.php
+++ admin/process/show_upload_navi_btns.php
@@ -3,15 +3,18 @@
 
 
 ?>
-<div  class=" bg-danger h-100 ">
-    <div class="flex text-end ">
-        <button>Cancel</button>
-    </div>
-    <div  class="h-100 bg-secondary d-flex flex-column align-items-center justify-content-between text-center">
-        <div class="bg-warning">
+<div class="row  h-100   p-0">
+    <div class="col-12 p-0">
 
-            <button class="px-2 py-2 ">upload audio samples</button>
-            <button class="px-2 py-2 ">upload midi files</button>
+        <div class="flex w-100 text-end ">
+            <button id="closeBtn" class="p-1"><img class="closeIcon" src="../../resources/icons/close_icon.png"></button>
+        </div>
+        <div class="h-100 w-100    d-flex   align-items-center justify-content-center ">
+            <div class=" gx-4 bg-danger px-3">
+
+                <button class="px-2 py-2 ">upload audio samples</button>
+                <button class="px-2 py-2 ">upload midi files</button>
+            </div>
         </div>
     </div>
 </div>
